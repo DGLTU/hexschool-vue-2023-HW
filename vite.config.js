3b19e57817f285b7bd8cb80df07891855b025bd2
DGLtu <dg56234@gmail.com>
2023-01-06 21:21:23 +0800

    Update baseUrl

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hexschool-vue-2023-HW/',
+  base: '/hexschool-vue-2023-HW/week2/',
   plugins: [vue()],
   resolve: {
     alias: {
